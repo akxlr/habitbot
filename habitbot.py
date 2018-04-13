9e9b797dd2d2edaee68fb88740603cbf373b01c0
Andrew Wrigley <andrew@wrigley.io>
2018-04-13 17:46:56 +0100

    Debugging

--- habitbot.py
+++ habitbot.py
@@ -191,15 +191,13 @@ def send_email(user, pwd, recipient, subject, body):
     # Prepare actual message
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
     """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
-    try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.ehlo()
-        server.starttls()
-        server.login(user, pwd)
-        server.sendmail(FROM, TO, message)
-        server.close()
-    except:
-        print("Failed to send email!")
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(user, pwd)
+    server.sendmail(FROM, TO, message)
+    server.close()
 
 
 def get_user_name(user_id):
